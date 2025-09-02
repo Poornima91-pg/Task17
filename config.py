81dcac01c46f3aff0a71313454c2b783436a8dc9
Poornima91-pg <poornimaravi11@gmail.com>
2025-09-02 18:39:52 +0530

    config file

---
+++ config.py
@@ -0,0 +1,9 @@
+
+# File contents username & password values
+
+VALID_USERNAME = "poornimaravi11@gmail.com"
+VALID_PASSWORD = "Vedapg@31"
+
+INVALID_USERNAME = "poornimaravi"
+INVALID_PASSWORD = "Ved31"
+
